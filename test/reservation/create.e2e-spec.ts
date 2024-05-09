7e9d19b35e2aa3b2cfb0fee2e80405ae0a6e7b0d
Luciano Martins <luciano.martinsjr@hotmail.com>
2024-05-08 21:40:21 -0300

    Update create.e2e-spec.ts

--- test/reservation/create.e2e-spec.ts
+++ test/reservation/create.e2e-spec.ts
@@ -2,42 +2,50 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, HttpStatus } from '@nestjs/common';
 import * as request from 'supertest';
 import { ReservationService } from '../../src/modules/reservation/reservation.service';
+import { ReservationController } from '../../src/modules/reservation/reservation.controller'; // Adjust this path to your actual controller path
 
 describe('ReservationController (e2e)', () => {
   let app: INestApplication;
-  let reservationService: ReservationService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [ReservationController], // Include the controller
+      providers: [ReservationService],
     })
       .overrideProvider(ReservationService)
       .useValue({
         create: jest.fn().mockResolvedValue({
             vehicleId: "1",
-            price:200,
-            initialDate:"2024-05-09",
-            endDate:"2024-05-10" }),
+            price: 200,
+            initialDate: "2024-05-09",
+            endDate: "2024-05-10"
+        }),
       })
       .compile();
 
     app = moduleFixture.createNestApplication();
-    reservationService = moduleFixture.get(ReservationService);
     await app.init();
   });
 
-  it('/vehicle (POST)', () => {
+  it('/reservation (POST)', async () => {
     return request(app.getHttpServer())
-      .post('/vehicle')
+      .post('/reservation')
       .send({ 
         vehicleId: "1",
-        price:200,
-        initialDate:"2024-05-09",
-        endDate:"2024-05-10"})
-      .expect(HttpStatus.CREATED)
-      .expect({ 
+        price: 200,
+        initialDate: "2024-05-09",
+        endDate: "2024-05-10"
+      })
+      .expect(HttpStatus.OK)
+      .expect({
         vehicleId: "1",
-        price:200,
-        initialDate:"2024-05-09",
-        endDate:"2024-05-10" });
+        price: 200,
+        initialDate: "2024-05-09",
+        endDate: "2024-05-10"
+      });
+  });
+
+  afterAll(async () => {
+    await app.close(); 
   });
 });
