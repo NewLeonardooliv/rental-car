5209c2428a9e23ef7c40eed66bf0cc7334ef8a98
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2024-05-11 11:38:02 -0300

    identation

--- src/modules/vehicle/vehicle.controller.spec.ts
+++ src/modules/vehicle/vehicle.controller.spec.ts
@@ -13,10 +13,29 @@ describe('VehicleController', () => {
         {
           provide: VehicleService,
           useValue: {
-            list: jest.fn().mockResolvedValue([{ id: '1', brand: 'Toyota', model: 'Corolla', year: 2022 }]),
-            findOne: jest.fn().mockResolvedValue({ id: '1', brand: 'Toyota', model: 'Corolla', year: 2022 }),
-            create: jest.fn().mockResolvedValue({ id: '1', brand: 'Toyota', model: 'Corolla', year: 2022 }),
-            save: jest.fn().mockResolvedValue({ id: '1', brand: 'Toyota', model: 'Corolla', year: 2023 }),
+            list: jest
+              .fn()
+              .mockResolvedValue([
+                { id: '1', brand: 'Toyota', model: 'Corolla', year: 2022 },
+              ]),
+            findOne: jest.fn().mockResolvedValue({
+              id: '1',
+              brand: 'Toyota',
+              model: 'Corolla',
+              year: 2022,
+            }),
+            create: jest.fn().mockResolvedValue({
+              id: '1',
+              brand: 'Toyota',
+              model: 'Corolla',
+              year: 2022,
+            }),
+            save: jest.fn().mockResolvedValue({
+              id: '1',
+              brand: 'Toyota',
+              model: 'Corolla',
+              year: 2023,
+            }),
             inativate: jest.fn().mockResolvedValue(true),
           },
         },
@@ -34,7 +53,9 @@ describe('VehicleController', () => {
   describe('list', () => {
     it('should return an array of vehicles', async () => {
       const result = await controller.list();
-      expect(result).toEqual([{ id: '1', brand: 'Toyota', model: 'Corolla', year: 2022 }]);
+      expect(result).toEqual([
+        { id: '1', brand: 'Toyota', model: 'Corolla', year: 2022 },
+      ]);
       expect(service.list).toHaveBeenCalled();
     });
   });
@@ -42,25 +63,50 @@ describe('VehicleController', () => {
   describe('find', () => {
     it('should return a single vehicle object', async () => {
       const result = await controller.find('1');
-      expect(result).toEqual({ id: '1', brand: 'Toyota', model: 'Corolla', year: 2022 });
+      expect(result).toEqual({
+        id: '1',
+        brand: 'Toyota',
+        model: 'Corolla',
+        year: 2022,
+      });
       expect(service.findOne).toHaveBeenCalledWith('1');
     });
   });
 
   describe('create', () => {
     it('should create and return the vehicle', async () => {
-      const dto = { brand: 'Toyota', model: 'Corolla', year: 2022, licensePlate: '2202AA2' };
+      const dto = {
+        brand: 'Toyota',
+        model: 'Corolla',
+        year: 2022,
+        licensePlate: '2202AA2',
+      };
       const result = await controller.create(dto);
-      expect(result).toEqual({ id: '1', brand: 'Toyota', model: 'Corolla', year: 2022 });
+      expect(result).toEqual({
+        id: '1',
+        brand: 'Toyota',
+        model: 'Corolla',
+        year: 2022,
+      });
       expect(service.create).toHaveBeenCalledWith(dto);
     });
   });
 
   describe('update', () => {
     it('should update and return the vehicle', async () => {
-      const dto = { brand: 'Toyota', model: 'Corolla', year: 2024, licensePlate: '220BB2' };
+      const dto = {
+        brand: 'Toyota',
+        model: 'Corolla',
+        year: 2024,
+        licensePlate: '220BB2',
+      };
       const result = await controller.update('1', dto);
-      expect(result).toEqual({ id: '1', brand: 'Toyota', model: 'Corolla', year: 2023 });
+      expect(result).toEqual({
+        id: '1',
+        brand: 'Toyota',
+        model: 'Corolla',
+        year: 2023,
+      });
       expect(service.save).toHaveBeenCalledWith('1', dto);
     });
   });
