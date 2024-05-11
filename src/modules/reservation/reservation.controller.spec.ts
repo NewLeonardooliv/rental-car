cce166d0184b46d5ce4e6f5347676809715f8de4
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2024-05-11 11:39:35 -0300

    fix. tests

--- src/modules/reservation/reservation.controller.spec.ts
+++ src/modules/reservation/reservation.controller.spec.ts
@@ -4,35 +4,50 @@ import { ReservationService } from './reservation.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 
 describe('ReservationController', () => {
-    let controller: ReservationController;
-    let service: ReservationService;
+  let controller: ReservationController;
+  let service: ReservationService;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [ReservationController],
-            providers: [
-                {
-                    provide: ReservationService,
-                    useValue: {
-                        create: jest.fn().mockResolvedValue({ }),
-                    },
-                },
-            ],
-        }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ReservationController],
+      providers: [
+        {
+          provide: ReservationService,
+          useValue: {
+            create: jest.fn().mockResolvedValue({
+              vehicleId: 'vehicleId',
+              initialDate: 'initialDate',
+              price: 150,
+              endDate: 'endDate',
+            }),
+          },
+        },
+      ],
+    }).compile();
 
-        controller = module.get<ReservationController>(ReservationController);
-        service = module.get<ReservationService>(ReservationService);
+    controller = module.get<ReservationController>(ReservationController);
+    service = module.get<ReservationService>(ReservationService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+  describe('create', () => {
+    it('should create and return the vehicle', async () => {
+      const dto: CreateReservationDto = {
+        vehicleId: 'vehicleId',
+        initialDate: 'initialDate',
+        price: 150,
+        endDate: 'endDate',
+      };
+      const result = await controller.create(dto);
+      expect(result).toEqual({
+        vehicleId: 'vehicleId',
+        initialDate: 'initialDate',
+        price: 150,
+        endDate: 'endDate',
+      });
+      expect(service.create).toHaveBeenCalledWith(dto);
     });
-
-    it('should be defined', () => {
-            expect(controller).toBeDefined();
-    }); 
-        describe('create', () => {
-            it('should create and return the vehicle', async () => {
-                const dto: CreateReservationDto = {vehicleId: 'vehicleId', initialDate: 'initialDate', price: 150, endDate: 'endDate' };
-                const result = await controller.create(dto);
-                expect(result).toEqual({vehicleId: 'vehicleId', initialDate: 'initialDate', price: 150,endDate: 'endDate'});
-                expect(service.create).toHaveBeenCalledWith(dto);
-            });
-    });
+  });
 });
